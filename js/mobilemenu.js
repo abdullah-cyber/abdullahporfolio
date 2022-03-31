872f417fcfa6a030427261743da634942ba5da39
abdullah-cyber <abdullahcyber87@gmail.com>
2022-03-31 16:18:45 +0100

    feat: add javascript code to my pop up

---
+++ js/mobilemenu.js
@@ -0,0 +1,25 @@
+function mobileMenu() {
+  if (document.getElementById('about').style.display == 'block') {
+    document.getElementsById('about').style.display = 'none';
+  } else {
+    document.getElementById('about').style.display = 'block';
+    document.getElementById('myOverlay').style.width = '100%';
+    document.body.style.position = 'fixed';
+  }
+}
+document.getElementById('hamburger').addEventListener('click', mobileMenu);
+function closeOverlay() {
+  document.getElementById('about').style.display = 'none';
+  document.getElementById('myOverlay').style.width = '0';
+  document.body.style.position = 'absolute';
+}
+document.getElementById('exitOverlay').addEventListener('click', closeOverlay);
+
+function resetMenu() {
+  if (window.innerWidth >= 900) {
+    document.getElementById('about').style.display = 'flex';
+  } else if (window.innerWidth < 900) {
+    document.getElementById('about').style.display = 'none';
+  }
+}
+window.addEventListener('resize', resetMenu);
