1ff462809fbde9ac40d5c083e01d7cb84230ea9c
abdullah-cyber <abdullahcyber87@gmail.com>
2022-05-05 16:18:02 +0100

    feat: update my validation

--- js/mobilemenu.js
+++ js/mobilemenu.js
@@ -6,6 +6,13 @@ const overlay = document.querySelector('.overlay');
 const exitOverlay = document.querySelector('.exitOverlay');
 const hamburger = document.querySelector('.hamburger1');
 const aboutmenu = document.querySelectorAll('.about-menu');
+const form = document.querySelector('form');
+const submitBtn = document.querySelector('#formSubmit');
+const fullname = document.querySelector('#fullname');
+const myEmail = document.querySelector('#email');
+const textarea = document.querySelector('#textarea');
+const icon = document.querySelectorAll('.myicon');
+const errorMessage = document.querySelectorAll('.error-text');
 
 const mobileMenu = () => {
   overlay.style.width = '100%';
@@ -84,3 +91,77 @@ window.onclick = function(event) {
     document.body.style.overflow = 'visible';
   }
 };
+
+//validation
+submitBtn.disabled = true;
+
+const fullnameChecker = () => {
+  icon[0].style.display = 'inline-block';
+
+  if (fullname.value === '') {
+    icon[0].style.display = 'none';
+    fullname.style.borderColor = '#ffffff';
+    errorMessage[0].style.display = 'none';
+    submitBtn.disabled = true;
+  } else if (fullname.value.length < 3) {
+    icon[0].innerHTML = '<i class="fa fa-exclamation-circle"></i>';
+    icon[0].style.color = '#e74c3c';
+    errorMessage[0].style.display = 'block';
+    fullname.style.borderColor = '#e74c3c';
+    submitBtn.disabled = true;
+  } else {
+    icon[0].innerHTML = '<i class="fas fa-check-circle"></i>';
+    icon[0].style.color = 'green';
+    fullname.style.borderColor = 'green';
+    errorMessage[0].style.display = 'none';
+  }
+};
+
+const emailChecker = () => {
+  icon[1].style.display = 'inline-block';
+  let emailRegX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (myEmail.value === '') {
+    icon[1].style.display = 'none';
+    myEmail.style.borderColor = '#ffffff';
+    errorMessage[1].style.display = 'none';
+    submitBtn.disabled = true;
+  } else if (!myEmail.value.match(emailRegX)) {
+    icon[1].innerHTML = '<i class="fa fa-exclamation-circle"></i>';
+    icon[1].style.color = '#e74c3c';
+    errorMessage[1].style.display = 'block';
+    myEmail.style.borderColor = '#e74c3c';
+    submitBtn.disabled = true;
+  } else {
+    icon[1].innerHTML = '<i class="fas fa-check-circle"></i>';
+    icon[1].style.color = 'green';
+    myEmail.style.borderColor = 'green';
+    errorMessage[1].style.display = 'none';
+  }
+};
+
+const textareaChecker = () => {
+  icon[2].style.display = 'inline-block';
+  if (textarea.value === '') {
+    icon[2].style.display = 'none';
+    textarea.style.borderColor = '#ffffff';
+    errorMessage[2].style.display = 'none';
+    submitBtn.disabled = true;
+  } else if (textarea.value.length < 15) {
+    icon[2].innerHTML = '<i class="fa fa-exclamation-circle"></i>';
+    icon[2].style.color = '#e74c3c';
+    errorMessage[2].style.display = 'block';
+    textarea.style.borderColor = '#e74c3c';
+    submitBtn.disabled = true;
+  } else {
+    icon[2].innerHTML = '<i class="fas fa-check-circle"></i>';
+    icon[2].style.color = 'green';
+    textarea.style.borderColor = 'green';
+    errorMessage[2].style.display = 'none';
+    submitBtn.disabled = false;
+  }
+};
+
+fullname.addEventListener('input', fullnameChecker);
+myEmail.addEventListener('input', emailChecker);
+textarea.addEventListener('input', textareaChecker);
